670c1c543b97d6983242905503e755e08f5319a6
CoderAriyan <111106728+CoderAriyan@users.noreply.github.com>
2022-09-13 03:35:17 +0600

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,15 @@
+// mixitUp
+
+var mixer = mixitup('.grid-portfolio-container');
+
+
+// scrollify
+
+$(function() {
+    $.scrollify({
+        section : ".scrlfy",
+        setHeights: false,
+        updateHash: false,
+        scrollSpeed: 1000
+    });
+});
