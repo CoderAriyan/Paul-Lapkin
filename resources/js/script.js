49d0a4c01b364eff105f27530d3b05003680ce51
CoderAriyan <111106728+CoderAriyan@users.noreply.github.com>
2022-09-19 00:26:35 +0600

    Add files via upload

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,9 +7,41 @@ var mixer = mixitup('.grid-portfolio-container');
 
 $(function() {
     $.scrollify({
-        section : ".scrlfy",
+        section : ".scrollify",
         setHeights: false,
         updateHash: false,
-        scrollSpeed: 1000
     });
 });
+
+
+
+// hamburger menu
+
+// open-menu
+
+function openMenu(){
+
+    document.getElementById('navbar').style.height = '100%';
+}
+
+// close-menu
+
+function closeMenu(){
+
+    document.getElementById('navbar').style.height = '0%';
+}
+
+
+// smoot-scrolls
+
+$(function(){
+
+    $('.menu-item a, .scroll-down a').on('click',function(){
+
+        $('html,body').animate({
+
+            scrollTop: $($.attr(this,'href')).offset().top
+        },1000);
+        return false;
+    });
+})
